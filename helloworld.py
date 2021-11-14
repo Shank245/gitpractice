43756e0b254b2898166ea712517e4d89462d747f
Shank245 <45233116+Shank245@users.noreply.github.com>
2021-11-14 14:02:55 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -2,4 +2,4 @@ Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (In
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> print("hello world")
 hello world
->>> 
+>>> print(helllow world)
